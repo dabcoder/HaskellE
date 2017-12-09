0609c13e6531ce0436ee315deda054c1f22cfba8
dabcoder <dbouchare@gmail.com>
2017-12-09 15:31:59 +0100

    Add FizzBuzz

--- cc.hs
+++ cc.hs
@@ -3,9 +3,8 @@ toDigits n
     | n <= 0    = []
     | otherwise = toDigits (n `div` 10) ++ [n `mod` 10]
 
-
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev n = reverse(toDigits n)
+toDigitsRev n = reverse (toDigits n)
 
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther []       = []
@@ -14,10 +13,13 @@ doubleEveryOther (x:y:zs) = x : 2*y : doubleEveryOther zs
 
 sumDigits :: [Integer] -> Integer
 sumDigits []      = 0
-sumDigits (x:xs)  = x + sumDigits xs
+sumDigits (x:[])  = x
+sumDigits (x:xs)
+    | n < 10    = x + sumDigits xs
+    | otherwise = sumDigits (toDigits x) + sumDigits xs
 
 validate :: Integer -> Bool
-validate n = doubleEveryOther (toDigits n)
+validate n = doubleEveryOther(toDigits n) 
 
 main = do
-    print $ toDigitsRev 5595
+    print $ toDigitsRev 5595 
