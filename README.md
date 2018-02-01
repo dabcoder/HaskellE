a1da923069643c7e8deeb7e78657ad342a7c551e
David Bouchare <david.bouchare@datadoghq.com>
2018-02-01 21:31:25 +0100

    Add function to stocks.hs

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Haskell experiments
 `cc.hs`: validate credit card numbers  
 `fizbuzz.hs`  
 `rockpaper.hs`: Rock paper scissors game  
+`stocks.hs`: Parsing JSON data from the iextrading API
--- stocks.hs
+++ stocks.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Lens
+import Data.Scientific as Scientific
 
 import Data.Aeson.Lens (_String, key)
 import Data.Aeson.Lens (_Number, key)
 import Network.Wreq
+
+stockWorthBuying :: Scientific -> String
+stockWorthBuying n 
+    | n <= 60 = "Worth buying perhaps?"
+    | n > 60  = "Too expensive!"
 
 -- builds the URL
 conditionsQuery2 :: String -> String
@@ -21,4 +27,6 @@ main = do
     print $ r ^. responseBody . key "companyName" . _String 
     putStrLn "And the stock price is: "
     print $ r ^?! responseBody . key "latestPrice" . _Number
+    let a = r ^?! responseBody . key "latestPrice" . _Number
+    putStrLn $ stockWorthBuying a
 
