4461f064d2b3bbc30184c54a9d194f0062d941b6
David Bouchare <david.bouchare@datadoghq.com>
2018-02-01 20:24:38 +0100

    Add api.iextrading example

---
+++ stocks.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Lens
+
+import Data.Aeson.Lens (_String, key)
+import Data.Aeson.Lens (_Number, key)
+import Network.Wreq
+
+-- builds the URL
+conditionsQuery2 :: String -> String
+conditionsQuery2 company =
+    "https://api.iextrading.com/1.0/stock/" ++ company ++ "/quote"
+
+-- User input for the company 
+main :: IO()
+main = do
+    print "What's the company name?"
+    company <- getLine
+    r <- get (conditionsQuery2 company)
+    putStrLn "The full company's name is: "
+    print $ r ^. responseBody . key "companyName" . _String 
+    putStrLn "And the stock price is: "
+    print $ r ^?! responseBody . key "latestPrice" . _Number
+
